cd2eae1ae276ff4f716bf06c6392c1868c8f852a
WGand <wgandino@gmail.com>
2021-01-11 00:51:07 -0400

    HOT FIX

--- index.js
+++ index.js
@@ -258,20 +258,20 @@ const postUsuario = async (request, response) => {
                           if (error) {
                             throw error;
                           }
-                        }
-                      )
-                      datosUsuario[0]['persona_contacto_celular']=results.rows[0]['numero_telefonico']
-                      datosUsuario[0]['persona_contacto_prefijo_celular']=results.rows[0]['prefijo']
-                      pool.query(
-                        'SELECT * FROM "TELEFONO" WHERE fk_persona_contacto = $1 AND prefijo =$2',
-                        [usuarioID, '0212'],
-                        (error, results) => {
-                          if (error) {
-                            throw error;
-                          }
-                          datosUsuario[0]['persona_contacto_telefono']=results.rows[0]['numero_telefonico']
-                          datosUsuario[0]['persona_contacto_prefijo_telefono']=results.rows[0]['prefijo']
-                          response.status(201).json(datosUsuario)
+                          datosUsuario[0]['persona_contacto_celular']=results.rows[0]['numero_telefonico']
+                          datosUsuario[0]['persona_contacto_prefijo_celular']=results.rows[0]['prefijo']
+                          pool.query(
+                            'SELECT * FROM "TELEFONO" WHERE fk_persona_contacto = $1 AND prefijo =$2',
+                            [usuarioID, '0212'],
+                            (error, results) => {
+                              if (error) {
+                                throw error;
+                              }
+                              datosUsuario[0]['persona_contacto_telefono']=results.rows[0]['numero_telefonico']
+                              datosUsuario[0]['persona_contacto_prefijo_telefono']=results.rows[0]['prefijo']
+                              response.status(201).json(datosUsuario)
+                            }
+                          )
                         }
                       )
                     }
@@ -1006,22 +1006,7 @@ const updateEmpleado = async (request, response) => {
               throw error;
             }
             actualizarTelefono(celular, telefono, prefijo, prefijo_celular, 'empleado')
-            pool.query('SELECT horario_id FROM "HORARIO" WHERE hora_inicio =$1 AND hora_fin =$2 and dia=$3',
-              [hora_inicio, hora_fin, dia],
-              (error, results) => {
-                if (error){
-                  throw error;
-                }
-                pool.query('SELECT horario_id FROM "HORARIO" WHERE hora_inicio =$1 AND hora_fin =$2 and dia=$3',
-                [hora_inicio, hora_fin, dia],
-                (error, results) => {
-                  if (error){
-                    throw error;
-                  }
-                }
-              )
-              }
-            )
+            response.status(201).json({ status: "Funciono", message: "Usuario registrado exitosamente" })
           }
         );
       }
@@ -1045,23 +1030,7 @@ const updateEmpleado = async (request, response) => {
               throw error;
             }
             actualizarTelefono(celular, telefono, prefijo, prefijo_celular, 'empleado')
-            pool.query('SELECT horario_id FROM "HORARIO" WHERE hora_inicio =$1 AND hora_fin =$2 and dia=$3',
-              [hora_inicio, hora_fin, dia],
-              (error, results) => {
-                if (error){
-                  throw error;
-                }
-                pool.query('INSERT INTO "EMPLEADO_HORARIO" (fk_empleado, fk_horario) VALUES ($1, $2)',
-                [rif, results.rows[0]['horario_id']],
-                (error, results) => {
-                  if (error){
-                    throw error;
-                  }
-                  response.status(201).json({ status: "Funciono", message: "Usuario registrado exitosamente" });    
-                }
-                )
-              }
-            )
+            response.status(201).json({ status: "Funciono", message: "Usuario registrado exitosamente" })
           }
         );
       }
@@ -1156,10 +1125,396 @@ const deleteNatural = async (request, response) => {
       if (error) {
         throw error;
       }
-      response.status(200).json(results.rowCount);
+      borrarTelefono(rif, 'natural')
+      pool.query(
+        'DELETE FROM "NATURAL" WHERE rif = $1',
+        [rif],
+        (error, results) => {
+          if (error) {
+            throw error;
+          }
+          response.status(200).json({ status: "Funciono", message: "Usuario registrado exitosamente" })
+        }
+      )
     }
-  );
-};
+  )
+}
+
+const deleteJuridico = async (request, response) => {
+  const { rif } = request.body;
+  pool.query(
+    'SELECT * FROM "JURIDICO" WHERE rif = $1',
+    [rif],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      borrarTelefono(rif, 'juridico')
+      pool.query('SELECT persona_id FROM "PERSONA_CONTACTO" WHERE fk_juridico = $1',
+      [rif],
+      (error, results) => {
+        if (error){
+          throw error;
+        }
+        persona_contacto_id = results.rows[0]['persona_id']
+        borrarTelefono(persona_contacto_id, 'persona_contacto')
+        pool.query('DELETE FROM "PERSONA_CONTACTO" WHERE fk_juridico = $1',
+        [rif],
+        (error, results) => {
+          if (error){
+            throw error;
+          }
+          pool.query('DELETE FROM "JURIDICO" WHERE rif = $1',
+          [rif],
+          (error, results) => {
+            if (error){
+              throw error;
+            }
+            response.status(201).json({ status: "success", message: "Funciono" })
+          }
+          )
+        }
+        )
+      }
+      )
+    }
+  )
+}
+
+const deleteEmpleado = async (request, response) => {
+  const { rif } = request.body;
+  pool.query(
+    'SELECT * FROM "EMPLEADO" WHERE rif = $1',
+    [rif],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      if(results.rowCount == 1){
+        pool.query(
+          'DELETE FROM "EMPLEADO_HORARIO" WHERE fk_empleado = $1',
+          [rif],
+          (error, results) => {
+            if (error) {
+              throw error;
+            }
+            pool.query(
+              'DELETE FROM "EMPLEADO" WHERE rif = $1',
+              [rif],
+              (error, results) => {
+                if (error) {
+                  throw error;
+                }
+              borrarTelefono(rif, 'empleado')
+              response.status(200).json({ status: "Funciono", message: "Usuario registrado exitosamente" })
+              }
+            )
+          }
+        )
+      }
+      else if(results.rowCount == 0){
+        response.status(200).json([])
+      }
+    }
+  )
+}
+
+const borrarTelefono = async(ID, tipo) =>{
+  switch(tipo){
+    case 'natural':
+      pool.query(
+        'DELETE FROM "TELEFONO" WHERE fk_natural = $1',
+        [ID],
+        (error, results) => {
+          if (error) {
+            throw error;
+          }
+        }
+      )
+      break
+    case 'juridico':
+      pool.query(
+        'DELETE FROM "TELEFONO" WHERE fk_natural = $1',
+        [ID],
+        (error, results) => {
+          if (error) {
+            throw error;
+          }
+        }
+      )
+      break
+    case 'persona_contacto':
+      pool.query(
+        'DELETE FROM "TELEFONO" WHERE fk_natural = $1',
+        [ID],
+        (error, results) => {
+          if (error) {
+            throw error;
+          }
+        }
+      )
+      break
+    case 'empleado':
+      pool.query(
+        'DELETE FROM "TELEFONO" WHERE fk_natural = $1',
+        [ID],
+        (error, results) => {
+          if (error) {
+            throw error;
+          }
+        }
+      )
+      break
+  }
+}
+
+const postTienda = async (request, response) =>{
+  const{
+    nombre,
+    fk_lugar
+  } = request.body
+  var tienda_id, zona_id, pasillo_id
+  pool.query(
+    'INSERT INTO "TIENDA" (nombre, fk_lugar) VALUES ($1, $2) RETURNING tienda_id',
+    [
+      nombre,
+      fk_lugar
+    ],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      tienda_id = results.rows[0]['tienda_id']
+      pool.query(
+        'INSERT INTO "ALMACEN" (nombre, cantidad, fk_tienda) VALUES ($1, $2, $3)',
+        [
+          'o',
+          100,
+          tienda_id
+        ],
+        (error, results) => {
+          if (error) {
+            throw error;
+          }
+          pool.query(
+            'INSERT INTO "ZONA" (nombre) VALUES ($1) RETURNING zona_id ',
+            [
+              'D'
+            ],
+            (error, results) => {
+              if (error) {
+                throw error;
+              }
+              zona_id = results.rows[0]['zona_id']
+              pool.query(
+                'INSERT INTO "ALMACEN_ZONA" (tipo, fk_zona, fk_almacen) VALUES ($1, $2, $3)',
+                [
+                  'REFRIGERADOS',
+                  zona_id,
+                  tienda_id
+
+                ],
+                (error, results) => {
+                  if (error) {
+                    throw error;
+                  }
+                  pool.query(
+                    'INSERT INTO "PASILLO" (nombre) VALUES ($1) RETURNING pasillo_id',
+                    [
+                      'F'
+                    ],
+                    (error, results) => {
+                      if (error) {
+                        throw error;
+                      }
+                      pasillo_id = results.rows[0]['pasillo_id']
+                      pool.query(
+                        'INSERT INTO "ZONA_PASILLO" (cantidad, fk_zona, fk_pasillo) VALUES ($1, $2, $3)',
+                        [
+                          100,
+                          zona_id,
+                          pasillo_id
+                        ],
+                        (error, results) => {
+                          if (error) {
+                            throw error;
+                          }
+                          response.status(201).json({message: "funciono"})
+                          
+                        }
+                      )
+                    }
+                  )
+                }
+              )
+            }
+          )
+        }
+      )
+    }
+  )
+}
+
+const deleteTienda = async (request, response) =>{
+  const{
+    nombre
+  } = request.body
+  var tienda_id, zona_id, pasillo_id
+  pool.query(
+    'SELECT * FROM "TIENDA" WHERE nombre=$1',
+    [
+      nombre
+    ],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      tienda_id = results.rows[0]['tienda_id']
+
+      pool.query(
+        'SELECT * FROM "ALMACEN_ZONA" WHERE fk_almacen = $1',
+        [
+          tienda_id
+        ],
+        (error, results) => {
+          if (error) {
+            throw error;
+          }
+          zona_id = results.rows[0]['fk_zona']
+          pool.query(
+            'SELECT * FROM "ZONA_PASILLO" WHERE fk_zona = $1',
+            [
+              zona_id
+            ],
+            (error, results) => {
+              if (error) {
+                throw error;
+              }
+              pasillo_id = results.rows[0]['fk_pasillo']
+              pool.query(
+                'DELETE FROM "ALMACEN_ZONA" WHERE fk_almacen = $1 and fk_zona',
+                [
+                  tienda_id,
+                  zona_id
+                ],
+                (error, results) => {
+                  if (error) {
+                    throw error;
+                  }
+                  pool.query(
+                    'DELETE FROM "ZONA_PASILLO" WHERE fk_pasillo = $1 and fk_zona',
+                    [
+                      pasillo_id,
+                      zona_id
+                    ],
+                    (error, results) => {
+                      if (error) {
+                        throw error;
+                      }
+                      pool.query(
+                        'DELETE FROM "PASILLO" WHERE fk_pasillo = $1',
+                        [
+                          pasillo_id
+                        ],
+                        (error, results) => {
+                          if (error) {
+                            throw error;
+                          }
+                          pool.query(
+                            'DELETE FROM "ZONA" WHERE fk_zona = $1',
+                            [
+                              zona_id
+                            ],
+                            (error, results) => {
+                              if (error) {
+                                throw error;
+                              }
+                              pool.query(
+                                'DELETE FROM "ALMACEN" WHERE almacen_id = $1',
+                                [
+                                  tienda_id
+                                ],
+                                (error, results) => {
+                                  if (error) {
+                                    throw error;
+                                  }
+                                  pasillo_id = results.rows[0]['fk_pasillo']
+                                  
+                                }
+                              )
+                              
+                            }
+                          )
+                          
+                        }
+                      )
+                      
+                    }
+                  )  
+                }
+              )
+            }
+          )
+        }
+      )
+    }
+  )
+}
+
+const updateTienda = async (request, response) =>{
+  const{
+    nombre,
+    fk_lugar
+  } = request.body
+  pool.query(
+    'INSERT INTO "TIENDA" (nombre, fk_lugar) VALUES ($1, $2)',
+    [
+      nombre,
+      fk_lugar
+    ],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).json(results);
+    }
+  )
+}
+
+const postProducto = async (request, response) => {
+  const {
+    imagen,
+    nombre,
+    precio,
+    UCABMART,
+    categoria
+  } = request.body
+  pool.query(
+    'INSERT INTO "PRODUCTO" (imagen, nombre, precio, UCABMART, categoria) VALUES ($1, $2, $3, $4, $5)',
+    [
+      imagen,
+      nombre,
+      precio,
+      UCABMART,
+      categoria
+    ],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).json(results);
+    }
+  )
+}
+
+app
+  .route("/tienda")
+  .post(postTienda)
+  .delete(deleteTienda)
+
+app
+  .route("/producto")
+
 
 app
   .route("/lugarparroquia")
@@ -1181,6 +1536,7 @@ app
   .route("/empleado")
   .post(postEmpleado)
   .put(updateEmpleado)
+  .delete(deleteEmpleado)
 
 app
   .route("/buscarLugar")
@@ -1203,10 +1559,12 @@ app
   .route("/usuarioNatural")
   .post(postNatural)
   .put(updateNatural)
+  .delete(deleteNatural)
 app
   .route("/usuarioJuridico")
   .post(postJuridico)
   .put(updateJuridico)
+  .delete(deleteJuridico)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
